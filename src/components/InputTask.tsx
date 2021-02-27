d7fdd2aa29e6e0bb214f59007990e9a6fdc45e4f
Syed Ali Uz Zaman <45460148+alyaxi@users.noreply.github.com>
2021-02-27 23:56:36 +0500

    addInput component

---
+++ src/components/InputTask.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import {useDispatch} from "react-redux";
+import {add} from "../reducer/TaskReducer";
+
+const InputTask = () => {
+    const dispatch = useDispatch();
+    const [input, setInput] = useState<string>("");
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+        if(input === " "){
+            dispatch(add({input: "Required Field"}))
+        }else {
+            dispatch(add({input}))
+            setInput("")
+        }
+    }
+
+    return (
+        <div>
+            <input type="text" value={input} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setInput(e.target.value)}}/>
+            <button onClick={handleSubmit}> ADD TASK</button>
+        </div>
+    )
+}
